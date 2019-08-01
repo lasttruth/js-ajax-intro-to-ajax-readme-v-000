3510ab1c9194d09e06ee1ad63edb0c8251b8a27e
Robin Doirin <robindoirin@gmail.com>
2019-08-01 16:57:38 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
 // your code here
 function showRepositories() {
 //this is set to the XMLHttpRequest object that fired the event
-  console.log(this.responseText);
-  let repoList = '<ul>';
-  for (var i = 0; i < this.responseText.length; i++) {
-    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-  }
-  repoList += '</ul>';
-  document.getElementById('repositories').innerHTML = repoList;
+var repos = JSON.parse(this.responseText);
+console.log(repos);
+const repoList = `<ul>${repos
+  .map(
+    r =>
+      '<li>' +
+      r.name +
+      ' - <a href="#" data-repo="' +
+      r.name +
+      '" onclick="getCommits(this)">Get Commits</a></li>'
+  )
+  .join('')}</ul>`;
+document.getElementById('repositories').innerHTML = repoList;
 }
 
 
